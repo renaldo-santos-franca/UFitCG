fa540d8dfa6a8ece13ae42f39d095aa456a3dab2
arlis filho <arlisfilho3@gmail.com>
2024-09-29 15:12:02 -0300

    mais coisas do menu

--- UFitCG/src/navegabilidade.pl
+++ UFitCG/src/navegabilidade.pl
@@ -1,6 +1,9 @@
 :- module(navegabilidade, [menuInicial/0]).
 
+clear_screen :- write('\e[H\e[2J').
+
 menuInicial :-
+    clear_screen,
     writeln("|------------------------------|\n|      Bem vindo à UFitCG      |\n|                              |\n|      Digite 1 para Login     |\n|      Digite 2 para Sair      |\n|------------------------------|\n"),
     read_line_to_codes(user_input, X3),
     string_to_atom(X3,X2),
@@ -10,4 +13,17 @@ menuInicial :-
 
 acaotelainicial(1) :- abalogin.
 acaotelainicial(2) :- halt.
-acaotelainicial(_) :- write("opção invalida")
+acaotelainicial(_) :- write("opção invalida"), menuInicial.
+
+abalogin :-
+    clear_screen,
+    writeln("LOGIN"),
+    write("Usuário: "),
+    read_line_to_codes(user_input, X3),
+    string_to_atom(X3,X2),
+    atom_string(X2,Login),
+    write("Senha: "),
+    read_line_to_codes(user_input, Y3),
+    string_to_atom(Y3,Y2),
+    atom_string(Y2,Senha),
+    halt.
